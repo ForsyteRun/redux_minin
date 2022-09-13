785d8f98de27da76e684b37bda91faaea39e9744
ForsyteRun <forsyterun@gmail.com>
2022-09-13 09:06:41 +0300

    pure formik + errors

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,45 +1,95 @@
 import React from "react";
 import style from './Register.module.css'
-import { Formik, Form, Field, ErrorMessage} from "formik";
+import { Formik, Form, Field } from "formik";
 import { validate } from "./formik/validateSchema";
 
 const initialValues = {
-   name: '', 
-   email: '',  
-   channel: '',
+   login: '', 
+   password: '',
 }
 
-const onSubmit = (element) => {
-   console.log(element);
+let onSubmit = (el) => {
+   console.log(el);
 }
 
 const Register = (props) => {
 
-   return (
+   return(
       <Formik
       initialValues = {initialValues}
       validationSchema = {validate}
       onSubmit = {onSubmit}>
-         <Form>
-            <div className={style.form_conteiner}>
-               <label for = 'name'>Name</label>
-               <Field type='text' id='name' name='name'/>
-               <ErrorMessage name='name'/>
-            </div>
-            <div className={style.form_conteiner}>
-               <label for = 'email'>E-mail</label>
-               <Field type='email' id='E-mail' name='email'/>   
-               <ErrorMessage name='email'/>
-            </div>
-            <div className={style.form_conteiner}>
-               <label for = 'channel'>Channel</label>
-               <Field type='text' id='channel' name='channel'/>
-               <ErrorMessage name = 'channel'/>
-            </div>
-            <input type='submit'/>
-         </Form>
+         {(props) => (
+            <Form name = 'form' id ='form'>
+               <div>
+                  <label htmlFor ='login'>login</label>
+                  <Field type ='text' name ='login' className={props.isValid || style.errors}/>
+                  {props.touched.login && <div className ={style.error}>{props.errors.login}</div>}
+               </div>
+               <div>
+                  <label htmlFor='rename'>password</label>
+                  <Field name='password' className={props.isValid || style.errors}/>
+                  {props.touched.password && <div className ={style.error}>{props.errors.password}</div>}
+               </div>
+               <button type='submit' disabled={!props.isValid}>Submit</button>
+            </Form>
+         )}
       </Formik>
    )
 }
 
 export default Register;
+
+
+
+// import React from 'react';
+// import { Field, Form, Formik, FormikProps } from 'formik';
+
+// const MyInput = ({ field, form, ...props }) => {
+//   return <input {...field} {...props} />;
+// };
+
+// const Register = () => (
+//   <div>
+//     <h1>My Form</h1>
+//     <Formik
+//       initialValues={{ email: '', color: 'red', firstName: '', lastName: '' }}
+//       onSubmit={(values, actions) => {
+//         setTimeout(() => {
+//           alert(JSON.stringify(values, null, 2));
+//           actions.setSubmitting(false);
+//         }, 1000);
+//       }}
+//     >
+//       {(props: FormikProps<any>) => (
+//         <Form>
+//           <Field type="email" name="email" placeholder="Email" />
+//           <Field as="select" name="color">
+//             <option value="red">Red</option>
+//             <option value="green">Green</option>
+//             <option value="blue">Blue</option>
+//           </Field>
+
+//           <Field name="lastName">
+//             {({
+//               field, // { name, value, onChange, onBlur }
+//               form: { touched, errors }, // also values, setXXXX, handleXXXX, dirty, isValid, status, etc.
+//               meta,
+//             }) => (
+//               <div>
+//                 <input type="text" placeholder="Email" {...field} />
+//                 {meta.touched && meta.error && (
+//                   <div className="error">{meta.error}</div>
+//                 )}
+//               </div>
+//             )}
+//           </Field>
+//           <Field name="lastName" placeholder="Doe" component={MyInput} />
+//           <button type="submit">Submit</button>
+//         </Form>
+//       )}
+//     </Formik>
+//   </div>
+// );
+
+//  export default Register;
--- src/formik/validateSchema.js
+++ src/formik/validateSchema.js
@@ -1,16 +1,8 @@
 import * as yup from 'yup';
 
 export const validate = yup.object().shape({
-   name: yup.string()
-   .min(4, 'Too Short!')
-   .max(24, 'Too Long!')
+   login: yup.string()
    .required('Required'),
- email: yup.string()
-   .min(6, 'Too short email')
-   .max(30, 'Too long')
-   .required('Required'),
- channel: yup.string()
-   .min(2, 'Too Short!')
-   .max(999999, 'Too Long!')
-   .required('Required'),
+   password: yup.string()
+   .required('required')
 })
