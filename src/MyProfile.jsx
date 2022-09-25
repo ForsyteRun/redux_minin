564a11accc184babe5dcc53912730a9d5adec87f
ForsyteRun <forsyterun@gmail.com>
2022-09-25 23:23:25 +0300

    download logo p.1

--- src/MyProfile.jsx
+++ src/MyProfile.jsx
@@ -1,21 +1,29 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 import photo from './img/smile.jpg';
+import style from './MyProfile.module.css';
 
 const MyProfile = (props) => {
-   
-   if(!props.isAuth){
-      return <Navigate to='/auth'/>
-   }
+
+   if (!props.isAuth) {
+      return <Navigate to='/auth' />
+   };
+
+   const onChangeAva = (e) => {
+     let logo = e.target.files[0];
+     console.log(logo);
+   };
 
    return (
-      <div>
-       {
-         props.isAuth ? <span>You autorized</span> : <span>Fail autorisation</span>
-       }  
-       <img src={photo}/>
+      <div className={style.conteiner}>
+         <div>
+            <img src={photo} style={{ width: '100px' }} />
+         </div>
+         <label className={style.button}>
+            <input type={"file"} onChange={onChangeAva}/>
+         </label>   
       </div>
    )
-}
+};
 
 export default MyProfile;
